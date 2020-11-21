2667998c4476de4709e6467b7dde9cdec03c4b58
Chad Green <chadgreen@chadgreen.com>
2020-11-20 20:56:33 -0500

    Initial EDA demo working

---
+++ Demos/Event-Driven Architecture/OrderSimulator/Program.cs
@@ -0,0 +1,45 @@
+﻿using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Producer;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderSimulator
+{
+	class Program
+	{
+		static async Task Main()
+		{
+
+			Console.WriteLine("Press any key to start sending events");
+			Console.ReadKey();
+
+			// Create a producer client that you can use to send events to an event hub
+			await using (var producerClient = new EventHubProducerClient(Settings.EventHubConnectionString, Settings.EventHubName))
+			{
+				// Create a batch of events
+				using EventDataBatch eventDataBatch = await producerClient.CreateBatchAsync();
+
+				// Add event to the batch. An event is represented by a collection of bytes and metadata.
+				eventDataBatch.TryAdd(new EventData(Encoding.UTF8.GetBytes(Guid.NewGuid().ToString())));
+				eventDataBatch.TryAdd(new EventData(Encoding.UTF8.GetBytes(Guid.NewGuid().ToString())));
+				eventDataBatch.TryAdd(new EventData(Encoding.UTF8.GetBytes(Guid.NewGuid().ToString())));
+
+
+				//if (eventDataBatch.TryAdd(new EventData(Encoding.UTF8.GetBytes(JsonSerializer.Serialize(order)))))
+				//	Console.WriteLine($"Added event for Order Id {order.Id}");
+
+				await producerClient.SendAsync(eventDataBatch);
+
+				Console.WriteLine($"Added {eventDataBatch.Count} event to the collective");
+
+				//Console.WriteLine("A batch of 3 events has been published.");
+
+			}
+
+			Console.WriteLine("All done");
+			Console.ReadLine();
+
+		}
+	}
+}
---
+++ Demos/Event-Driven Architecture/OrderSimulator/Settings.cs
@@ -0,0 +1,9 @@
+﻿namespace OrderSimulator
+{
+	public static class Settings
+	{
+		public const string EventHubConnectionString = "Endpoint=sb://dallasaustin.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=0WTvZX+/W4sbfjSN+Xb5xTCnsuxpq2/RIHtQ9bjUkIY=";
+		public const string EventHubName = "OrderProcessing";
+	}
+
+}
