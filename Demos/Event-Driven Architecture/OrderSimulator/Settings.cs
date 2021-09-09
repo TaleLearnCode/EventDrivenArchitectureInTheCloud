573c8794456ff98f1bfae72cd2a67976561b4a43
Chad Green <chad.green@talelearncode.com>
2021-09-09 12:27:59 -0400

    Complete for DevSpace

--- Demos/Event-Driven Architecture/OrderSimulator/Settings.cs
+++ Demos/Event-Driven Architecture/OrderSimulator/Settings.cs
@@ -2,7 +2,7 @@
 {
 	public static class Settings
 	{
-		public const string EventHubConnectionString = "Endpoint=sb://dallasaustin.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=0WTvZX+/W4sbfjSN+Xb5xTCnsuxpq2/RIHtQ9bjUkIY=";
+		public const string EventHubConnectionString = "Endpoint=sb://eventdrivenarchitectureinthecloud.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=cpxMz3mjmmuV320OuCse8xPBKEXAWfhGGxxfNN19UgQ=";
 		public const string EventHubName = "OrderProcessing";
 	}
 
